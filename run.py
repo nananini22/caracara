d480ea49fb814ac11f78d6bb2058c8ba79f27fe4
kei <ccychacon@gmail.com>
2018-10-28 05:58:59 -0600

    error

--- run.py
+++ run.py
@@ -45,36 +45,36 @@ def kairos(crop, cuantos):
     cv2.imwrite("faces/{}.jpg".format(unique_filename), crop)
     cv2.imwrite("faces/face.jpg".format(unique_filename), crop)
     recognized_faces = kairos_face.detect_face(file="faces/face.jpg")
-    edad = recognized_faces['images'][0]['faces'][0]['attributes']['age']
-    sexo = recognized_faces['images'][0]['faces'][0]['attributes']['gender']['type']
-    if sexo == 'M':
-        sexo = "HOMBRE"
-    else:
-        sexo = "MUJER"
-    print("edad: {}, sexo: {}".format(edad, sexo))
-    now = datetime.datetime.now()
-    img = cv2.imread('p3.png',1)
-    font = cv2.FONT_HERSHEY_DUPLEX
-    hora = str(now.hour) + ":" + str(now.minute)
-    age = str(edad)
+    if recognized_faces != "Error":
+        edad = recognized_faces['images'][0]['faces'][0]['attributes']['age']
+        sexo = recognized_faces['images'][0]['faces'][0]['attributes']['gender']['type']
+        if sexo == 'M':
+            sexo = "HOMBRE"
+        else:
+            sexo = "MUJER"
+        print("edad: {}, sexo: {}".format(edad, sexo))
+        now = datetime.datetime.now()
+        img = cv2.imread('p3.png',1)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        hora = str(now.hour) + ":" + str(now.minute)
+        age = str(edad)
 
-    cv2.putText(img, hora, (950, 510), font, 1.7, (0, 0, 255), 2)
-    cv2.putText(img, sexo, (680, 583), font, 1.7, (0, 0, 255), 2)
-    cv2.putText(img, age, (870, 663), font, 1.7, (0, 0, 255), 2)
+        cv2.putText(img, hora, (950, 510), font, 1.7, (0, 0, 255), 2)
+        cv2.putText(img, sexo, (680, 583), font, 1.7, (0, 0, 255), 2)
+        cv2.putText(img, age, (870, 663), font, 1.7, (0, 0, 255), 2)
 
-    s_img = cv2.imread("faces/face.jpg")
-    s_img2 = cv2.resize(s_img,(int(443),int(590)))
-    cv2.imwrite("faces/face2.jpg", s_img2)
-    s_img3 = cv2.imread("faces/face2.jpg")
-    cv2.imwrite("faces/face2.jpg", s_img2)
-
-
-    x_offset=0
-    y_offset=210
-    img[y_offset:y_offset+590, x_offset:x_offset+443] = s_img3
-    cv2.imwrite("faces/anuncio.jpg", img)
-    from subprocess import call
-    #call(["curl" ,"-F", "sampleFile=@p2.png", "http://localhost:3000/upload"])
+        s_img = cv2.imread("faces/face.jpg")
+        s_img2 = cv2.resize(s_img,(int(443),int(590)))
+        cv2.imwrite("faces/face2.jpg", s_img2)
+        s_img3 = cv2.imread("faces/face2.jpg")
+        cv2.imwrite("faces/face2.jpg", s_img2)
+        x_offset=0
+        y_offset=210
+        img[y_offset:y_offset+590, x_offset:x_offset+443] = s_img3
+        cv2.imwrite("faces/anuncio.jpg", img)
+        from subprocess import call
+        #call(["curl" ,"-F", "sampleFile=@p2.png", "http://localhost:3000/upload"])
+        
 
 
 framecount = 0
